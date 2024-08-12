49d700feef9a1f353bd1116f5fb019d9156d5829
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2024-08-12 20:20:41 +0300

    Docker Compose Files Added. Controller Authorization Completed.

---
+++ Infrastructure/Papara.CaptainStore.Persistence/Configurations/AppRoleConfigurations/AppRoleConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Papara.CaptainStore.Domain.Entities.AppRoleEntities;
+
+namespace Papara.CaptainStore.Persistence.Configurations.AppRoleConfigurations
+{
+    public class AppRoleConfiguration
+    {
+        public void Configure(EntityTypeBuilder<AppRole> builder)
+        {
+            builder.HasKey(role => role.Id);
+            builder.HasIndex(role => role.Name).IsUnique();
+            builder.HasIndex(role => role.NormalizedName).IsUnique();
+        }
+    }
+}
